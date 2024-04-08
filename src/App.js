b0ad46fdde992e4c4921b6ab4c38ef6b89b60b98
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-04-08 21:49:35 +0200

    first class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import React from "react";
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return <div className="App"></div>;
+    this.state = { count: 5 };
+  }
+
+  render() {
+    return (
+      <div>
+        <button>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;

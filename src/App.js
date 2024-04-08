5b8f02dec1659ce38576bd0f82aab6d2f5100995
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-04-08 22:05:43 +0200

    event handlers

--- src/App.js
+++ src/App.js
@@ -4,14 +4,37 @@ class Counter extends React.Component {
     super(props);
 
     this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    console.log(this);
+    this.setState((currentState) => {
+      return { count: currentState.count - 1 };
+    });
+
+    //sets value straight to 10
+    // this.setState({ count: 10 });
+  }
+
+  handleIncrement() {
+    this.setState((currentState) => {
+      return { count: currentState.count + 1 };
+    });
   }
 
   render() {
+    const date = new Date("june 20 2023");
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
